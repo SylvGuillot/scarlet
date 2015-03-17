adc20a19026ec355c6355014b36511922e2b7dab
SylvGuillot <guillot.sylv@gmail.com>
2015-03-17 09:37:22 +0100

    Update Salt Keys wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -48,14 +48,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'JvQ;0#n7{1# y(/=F|7 /oG0:/L]N IuB+J8!Xk,S_j;t<D=OTZ3T,kaSH`CRc0W');
+define('SECURE_AUTH_KEY',  'u+}@g.)|gqUEwLv4Xj4AN.|l&;H%}2Xx]99Z]H+6}jQq9<F=e+Nh,3zT*e(2(h1+');
+define('LOGGED_IN_KEY',    'NV-CYCuCDV;V7-aS=V+J!9Zw3.Ev_B.m(O-qt t<r$f7U<-H(`{|+c`Y|ti&#Kc2');
+define('NONCE_KEY',        'Org8C2*$ffIgEfxY )Vb!?V(h>Up(<zAP!RJ9l)(>#vu;5=&ut2_b7*5SO)FP@V(');
+define('AUTH_SALT',        'CP5*D>j;ab H|ugeNM;V_QJ-A[Bd!m|74?|UuYBGD+1D#f#_U=_F+1>:7DuF2@3h');
+define('SECURE_AUTH_SALT', '01MC#yVXd6E1<fF?-iR<)dE$y|s~:QCH*2+{T}`z9LRIS+@V<*y0$IC9#PBop,Yw');
+define('LOGGED_IN_SALT',   'j=GMk5%S%bYj9Yl6[ayV#_JHAU$52~6sgz}q3e^#@]0[ot- 4e/wjr~RdZm}yu+L');
+define('NONCE_SALT',       'N`A&%ktIu3SN*N7!l~AJg;^h@TZ#qJ15?=eBP&R(u=k#3i|I77&YxxNf#T_-8~8F');
 
 /**#@-*/
 

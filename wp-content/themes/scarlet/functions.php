983d8ed5ae48d6d342605688b944e950ac3c3323
mounce <poncemau.mmi@gmail.com>
2015-03-20 13:52:03 +0100

    updates f & h

--- wp-content/themes/scarlet/functions.php
+++ wp-content/themes/scarlet/functions.php
@@ -1,7 +1,5 @@
 <?php
 
 add_theme_support("menus");
-require_once locate_template ( '/lib/scripts.php' ); // Scripts and stylesheets
-
-
+require_once locate_template ( '/scripts.php' ); // Scripts and stylesheets
 ?>
--- wp-content/themes/scarlet/header.php
+++ wp-content/themes/scarlet/header.php
@@ -2,15 +2,50 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Scarlet</title>
+    <title>Document</title>
     
-    <?php wp_head(); ?>
+     <link href="style.css" rel="stylesheet">
     
 </head>
 <body>
-    
+
 <header>
-    <h1>Scarlet</h1>
-    <?php wp_nav_menu(array('menu' => 'main')) ?>
+
+<div id="cover">
+    <img src="img/SunRise.png" alt="">
+    
+    <div id="first_link"> </div>
+    
+    <div id="second_link"> </div>
+    
+    <div id="third_link"> </div>
+    
+    <div id="fourth_link"> </div>
+    
+    <div id="fifth_link"> </div>
+</div>
+    
+<div id="nav_tool">
+    <div id="home_link"> </div>
+    <div id="search_bar"> </div>
+    <div id="languages"> </div>
+</div>
+   
+<div id="heading">
+    <ul>
+        <li class="section">News</li>
+        <li class="section">Le Projet</li>
+        <li class="section">L'équipe</li>
+        <li class="section">La Voiture</li>
+        <li class="section">Médias</li>
+        <li class="section">Partenaires</li>
+        <li class="section">Contact</li>
+    </ul>
+    
+</div>
     
 </header>
+
+        
+</body>
+</html>
